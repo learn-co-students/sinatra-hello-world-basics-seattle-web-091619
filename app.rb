326483e523056d8209d676a5d52d231a9509474e
github username <github email address>
2019-10-08 09:54:25 -0700

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
     "Hello, World!"
   end
 
+  get '/name' do
+    "My name is Daniel"
+  end
+
+  get '/hometown' do
+    "My hometown is OB"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is beat it"
+  end
+
+
+
+
+
+
+
+
 
 end
